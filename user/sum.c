52e289bc33e7abbe19b0053fd5e0c0a5546e43cf
Mariia <marialembomail@gmail.com>
2024-02-19 07:53:23 +0300

    Add syscall

--- user/sum.c
+++ user/sum.c
@@ -2,46 +2,64 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(void) {
-    char buf[100];
-    int n, total = 0;
-    while((n = read(0, buf + total, 1)) > 0) {
-        if(buf[total] == '\n') {
-            buf[total] = '\0';
-            break;
-        }
-        total += n;
-        if(total >= sizeof(buf) - 1) break;
-    }
-
-    if(n < 0) {
-        printf("Ошибка чтения\n");
-        exit(1);
-    }
-
-    // Проверка на пустую строку или строку, состоящую только из символа новой строки
-    if(total == 0) {
-        printf("Ошибка: ввод пустой строки.\n");
-        exit(1);
-    }
-
-    printf("|%s|\n", buf); // Тестовый вывод
-
-    // Проверка на наличие только допустимых символов (цифры и пробелы)
-    for(int i = 0; i < total; ++i) {
-        if((buf[i] < '0' || buf[i] > '9') && buf[i] != ' ') {
-            printf("Ошибка: некорректный символ во вводе.\n");
-            exit(1);
-        }
-    }
-
-    int a, b;
-    if(sscanf(buf, "%d %d", &a, &b) == 2) {
-        printf("Сумма: %d\n", a + b);
-    } else {
-        printf("Ошибка: некорректный формат ввода.\n");
-    }
-
-    exit(0);
+int isNumericChar(char c) {
+    return (c != '\0' && (c < '0' || c > '9'));
 }
 
+int main(int argc, char *argv[]) {
+    char inputBuffer[32];
+    char *currentChar = inputBuffer;
+    char *separator = inputBuffer;
+    int bytesRead;
+
+    while (1) {
+        bytesRead = read(0, currentChar++, 1);
+
+        switch (bytesRead) {
+            case 0:
+                printf("error: reached the end of the current file\n");
+                exit(1);
+            case -1:
+                printf("error: couldn't read\n");
+                exit(1);
+        }
+
+        if (currentChar - inputBuffer == sizeof inputBuffer) {
+            printf("Buffer overflow\n");
+            exit(1);
+        }
+
+        if (*(currentChar - 1) == '\n') {
+            *(currentChar - 1) = '\0';
+            break;
+        }
+    }
+
+    while (*separator != ' ' && separator - inputBuffer < sizeof inputBuffer) {
+        ++separator;
+    }
+
+    if (separator - inputBuffer == sizeof inputBuffer) {
+        printf("error: couldn't find the space\n");
+        exit(2);
+    }
+    *separator = '\0';
+    ++separator;
+
+    if (separator - inputBuffer < 2 || currentChar - separator < 2) {
+        printf("error: couldn't find the second number\n");
+        exit(2);
+    }
+
+    for (char *tmp = inputBuffer; tmp < currentChar; ++tmp) {
+        if (isNumericChar(*tmp)) {
+            printf("error: couldn't parse the numbers\n");
+            exit(2);
+        }
+    }
+
+    int firstNumber = atoi(inputBuffer); int secondNumber = atoi(separator);
+
+    printf("%d\n", firstNumber + secondNumber);
+    exit(0);
+}
