ba221355f6799341593828a47e84f17666224e2d
Mariia <marialembomail@gmail.com>
2024-02-19 07:02:26 +0300

    Add syscall

--- user/sum.c
+++ user/sum.c
@@ -2,31 +2,67 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(void) {
-    char buf[100];
-    printf("Введите два числа, разделенные пробелом: ");
+#define MAX_INPUT_SIZE 32
+
+void parseInput(char *input, int *firstNum, int *secondNum);
+int findSpaceAndCheck(char *start, char *end);
+
+int main(int argc, char *argv[]) {
     
-    // Изменение с gets на fgets для предотвращения переполнения буфера
-    if (fgets(buf, sizeof(buf), stdin) == NULL) {
-        printf("Ошибка чтения.\n");
-        exit(1);
-    }
-   
-    int len = strlen(buf);
-    if (buf[len - 1] == '\n') {
-        buf[len - 1] = '\0';
-    } else {
-        printf("Введенная строка слишком длинная.\n");
-        exit(1);
+    char inputBuffer[MAX_INPUT_SIZE];
+    char *currentChar = inputBuffer;
+    char *spacePointer = inputBuffer;
+
+    gets(inputBuffer, sizeof(inputBuffer) - 1);
+
+    // Find the newline character
+    for (; currentChar < inputBuffer + sizeof(inputBuffer) - 1 && *currentChar != '\n'; ++currentChar);
+    ++currentChar;
+    *(currentChar - 1) = '\0';
+
+    if (!findSpaceAndCheck(spacePointer, currentChar)) {
+        printf("Incorrect format: couldn't find space\n");
+        exit(2);
     }
 
-    int a, b;
-    if (sscanf(buf, "%d %d", &a, &b) == 2) {
-        printf("Сумма: %d\n", a + b);
-    } else {
-        printf("Ошибка: некорректный ввод. Убедитесь, что вы ввели два числа, разделенные пробелом.\n");
-        exit(1);
+    // Check if there is at least one digit before and after the space
+    if (spacePointer - inputBuffer < 2 || currentChar - spacePointer < 2) {
+        printf("Incorrect format: couldn't find one of the numbers\n");
+        exit(2);
     }
+
+    // Check if both parts of the input are numeric
+    for (char *tmp = inputBuffer; tmp < currentChar; ++tmp) {
+        if (isNumericChar(*tmp)) {
+            printf("Incorrect format: couldn't parse two numbers\n");
+            exit(2);
+        }
+    }
+
+    int firstNumber, secondNumber;
+    parseInput(inputBuffer, &firstNumber, &secondNumber);
+
+    printf("%d\n", add(firstNumber, secondNumber));
 
     exit(0);
+}
+
+void parseInput(char *input, int *firstNum, int *secondNum) {
+    char *separator = input;
+    for (; *separator != '\0'; ++separator);
+
+    *separator = '\0';
+    ++separator;
+
+    *firstNum = atoi(input);
+    *secondNum = atoi(separator);
+}
+
+int isNumericChar(char c) {
+    return (c != '\0' && (c < '0' || c > '9'));
+}
+
+int findSpaceAndCheck(char *start, char *end) {
+    for (; *start != ' ' && start < end; ++start);
+    return (start != end);
 }
